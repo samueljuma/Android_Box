0c04525b20ac40e938914247b653828bfd875484
samueljuma <samueljuma1024@gmail.com>
2021-05-14 15:01:52 +0300

    Dice Roll lateinit(best practices)

--- DiceRoller/app/src/main/java/com/devjay/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/devjay/diceroller/MainActivity.kt
@@ -10,20 +10,25 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage :ImageView
+    lateinit var rollButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.roll_button)
+
+        // roll button functionality
+        rollButton = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
             rollDice()
         }
+        diceImage = findViewById(R.id.dice_image);
     }
 
     private fun rollDice() {
         //generate a random number between 1 and 6
-        val randomInt = Random.nextInt(6) + 1
-        val diceImage : ImageView = findViewById(R.id.dice_image)
-        val drawableResource = when (randomInt) {
+             //inline variable
+        val drawableResource = when (Random.nextInt(6) + 1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
