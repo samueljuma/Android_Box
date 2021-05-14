2436c8d669607d40fb51a8dad275d6dfdbb93723
samueljuma <samueljuma1024@gmail.com>
2021-05-14 13:50:41 +0300

    DiceRoller roll button

--- DiceRoller/app/src/main/java/com/devjay/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/devjay/diceroller/MainActivity.kt
@@ -2,11 +2,18 @@ package com.devjay.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
