e5d3347d746a24ddc859edfd249ca2dac3a69fe9
samueljuma <samueljuma1024@gmail.com>
2021-05-16 14:48:38 +0300

    makecolored: Databinding is awesome

--- ColorMyViews/app/src/main/java/com/devjay/colormyviews/MainActivity.kt
+++ ColorMyViews/app/src/main/java/com/devjay/colormyviews/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.devjay.colormyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.devjay.colormyviews.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +15,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        setListeners()
+    }
+    private fun setListeners() {
+        val clickableViews:List<View> = listOf(box_one,box_two,
+            box_three,box_four,box_five)
+        for(item in clickableViews){
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+    private fun makeColored(view: View) {
+        binding.apply {
+            when (view.id){
+                //colors from the color class
+                box_one.id -> view.setBackgroundColor(Color.DKGRAY)
+                box_two.id -> view.setBackgroundColor(Color.GRAY)
+                //colors from the Android color resource
+                box_three.id -> view.setBackgroundResource(android.R.color.holo_green_light)
+                box_four.id -> view.setBackgroundResource(android.R.color.holo_green_dark)
+                box_five.id -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+                else -> view.setBackgroundColor(Color.LTGRAY)
+            }
+        }
+
+
     }
 }
