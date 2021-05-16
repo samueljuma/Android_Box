ba69fe1ab52c132ef3dcca2acd4efcea19275e40
samueljuma <samueljuma1024@gmail.com>
2021-05-16 11:48:31 +0300

    ColorMyViews: initial commit

---
+++ ColorMyViews/app/src/main/java/com/devjay/colormyviews/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.devjay.colormyviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.devjay.colormyviews.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+    }
+}
