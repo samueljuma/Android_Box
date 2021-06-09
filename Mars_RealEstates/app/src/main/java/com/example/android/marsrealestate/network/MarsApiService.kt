a483b89af622ea0cf8d3fddae022d0d6966fab2f
samueljuma <samueljuma1024@gmail.com>
2021-06-09 13:03:27 +0300

    implemented moshi

--- Mars_RealEstates/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ Mars_RealEstates/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,8 +17,11 @@
 
 package com.example.android.marsrealestate.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
@@ -27,8 +30,14 @@ private const val BASE_URL = "https://mars.udacity.com/"
 /**
  * Create a Retrofit object
  */
+private val moshi =Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+/**
+ * Create a Retrofit object
+ */
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
@@ -37,8 +46,7 @@ private val retrofit = Retrofit.Builder()
  */
 interface MarsApiService {
     @GET("realestate")
-    fun getProperties(): Call<String>
-
+    fun getProperties(): Call<List<MarsProperty>>
 }
 
 /**
