e90cb951925bc5cc6307e73ce3831cd9c6f980f4
samueljuma <samueljuma1024@gmail.com>
2021-06-09 15:55:44 +0300

    corotines instead of callbacks

--- Mars_RealEstates/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ Mars_RealEstates/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,12 +17,13 @@
 
 package com.example.android.marsrealestate.network
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.Deferred
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://mars.udacity.com/"
@@ -38,6 +39,7 @@ private val moshi =Moshi.Builder()
  */
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
     .baseUrl(BASE_URL)
     .build()
 
@@ -46,7 +48,7 @@ private val retrofit = Retrofit.Builder()
  */
 interface MarsApiService {
     @GET("realestate")
-    fun getProperties(): Call<List<MarsProperty>>
+    fun getPropertiesAsync(): Deferred<List<MarsProperty>>
 }
 
 /**
--- Mars_RealEstates/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ Mars_RealEstates/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -22,6 +22,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.android.marsrealestate.network.MarsApi
 import com.example.android.marsrealestate.network.MarsProperty
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -38,6 +42,9 @@ class OverviewViewModel : ViewModel() {
     val response: LiveData<String>
         get() = _response
 
+    //coroutine job and scope
+    private var viewModelJob = Job()
+    private val coroutineScope = CoroutineScope(viewModelJob +Dispatchers.Main)
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
      */
@@ -49,15 +56,20 @@ class OverviewViewModel : ViewModel() {
      * Sets the value of the status LiveData to the Mars API status.
      */
     private fun getMarsRealEstateProperties() {
-        MarsApi.retrofitService.getProperties().enqueue(object : Callback<List<MarsProperty>> {
-            override fun onFailure(call: Call<List<MarsProperty>>, t: Throwable) {
+
+        coroutineScope.launch {
+            var getPropertiesDeferred = MarsApi.retrofitService.getPropertiesAsync()
+            try {
+                var listResult = getPropertiesDeferred.await()
+                _response.value = "Success: ${listResult.size} MarsProperties Retrieved "
+            }catch (t: Throwable){
                 _response.value = "Failure:" + t.message
             }
-            override fun onResponse(call: Call<List<MarsProperty>>,
-                response: Response<List<MarsProperty>>) {
-                _response.value = "Success: ${response.body()?.size} MarsProperties Retrieved "
-            }
-        })
-        _response.value = "Set the Mars API Response here!"
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
     }
 }
